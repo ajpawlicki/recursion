13e8dd0dc7c9f16f145d2e63dd8f372efbb3e8e5
AJ Pawlicki <aloysiuspaw@gmail.com>
2017-01-26 15:10:57 -0800

    Outline recursive and base cases

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,18 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+	var result = '';
+
+	// base case
+	// typeOf = string or number
+	// condition to find base case: object or array has no more nest
+
+	if ()
+
+	// recursive case
+	// typeOf = object
+	// if array then have to include []
+	// if object then have to include {}
+	
+	return result;
 };
